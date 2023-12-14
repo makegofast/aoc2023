388601e4d282d10d6581897d96d3bc87a5d3f309
Joe Webster <joe@makegofast.com>
2023-12-14 22:11:27 +0000

    Day 11 Part 2: It broke part 1 but ZFG

--- python/days/11/solution.py
+++ python/days/11/solution.py
@@ -1,40 +1,34 @@
 import math
+galaxies = {}
+row_expansion = {}
+col_has_galaxies = {}
 
-def read_file(filename, part2=False):
-    galaxies = []
-    map = []
+def read_file(filename):
+    global galaxies, row_expansion, col_has_galaxies
 
-    col_data = {}
+    row_expansion = {}
+    col_has_galaxies = {}
+
+    gid = 0
     for row, line in enumerate(open(filename).read().splitlines()):
-        map.append([c for c in line])
-
         if '#' not in line:
-            map.append(map[-1].copy())
-
-        for col, cnt in {i: 1 for i, v in enumerate(map[-1]) if v=="#"}.items():
-            col_data.setdefault(col, 0)
-            col_data[col] += 1
-    
-    for i in range(len(map[-1])-1, 0, -1):
-        if i not in col_data:
-            for r, m in enumerate(map):
-                map[r].insert(i, '.')
-
-    for row, line in enumerate(map):
+            row_expansion[row] = True 
+        
         for col, c in enumerate(line):
             if c == "#":
-                galaxies.append([row, col])
-
-    return map, galaxies
+                gid += 1
+                col_has_galaxies[col] = True
+                galaxies[gid] = [row, col]
     
-def cal_distances(map, galaxies):
+def cal_distances(expansion_factor):
+    global galaxies, col_has_galaxies
     seen = {}
     distances = []
 
-    for sgid, sc in enumerate(galaxies):
-        if sgid % 10 == 0:
-            print(f"{sgid}/{len(galaxies)} {math.floor(sgid/len(galaxies)*100)}%")
-        for dgid, dc in enumerate(galaxies):
+    for sgid, sc in galaxies.items():
+        #if sgid % 100 == 0:
+        #    print(f"{sgid}/{len(galaxies)} {math.floor(sgid/len(galaxies)*100)}%")
+        for dgid, dc in galaxies.items():
             if sgid == dgid:
                 #print(f"skipping {sgid}->{dgid} as they are the same")
                 continue
@@ -43,13 +37,18 @@ def cal_distances(map, galaxies):
                 #print(f"skipping {sgid}->{dgid} as we've already seen the reverse {dgid}->{sgid}")
                 continue
 
-            d = abs(dc[0]-sc[0]) + abs(dc[1]-sc[1]) 
-            #print(f"cal {sgid+1}->{dgid+1} = {d}")
+            rs = min(sc[0], dc[0])
+            re = max(sc[0], dc[0])
+            cs = min(sc[1], dc[1])
+            ce = max(sc[1], dc[1])
+            row_exp = sum([expansion_factor if i in row_expansion else 1 for i in range(rs, re)])
+            col_exp = sum([expansion_factor if i not in col_has_galaxies else 1 for i in range(cs, ce)])
+            d = row_exp + col_exp
+            #print(f"cal {sgid}->{dgid} = {d} (sc={sc} dc={dc} rs={rs} re={re} cs={cs} ce={ce} row_exp={row_exp}, col_exp={col_exp})")
             distances.append(d)
 
             seen[f"{sgid}->{dgid}"] = True
 
-    print("sum:", sum(distances))
     return sum(distances)
             
 def print_map(map):
@@ -59,9 +58,14 @@ def print_map(map):
             print(c, end="")
         print()
 
-def solve(filename, part2=False):
-    map, galaxies = read_file(filename, part2)
-    sum_d = cal_distances(map, galaxies)
+def solve(filename, expansion_factor=1):
+    global galaxies, row_expansion, col_has_galaxies
+    galaxies = {}
+    row_expansion = {}
+    col_has_galaxies = {}
+
+    read_file(filename)
+    sum_d = cal_distances(expansion_factor)
     return sum_d
 
 
@@ -69,5 +73,6 @@ if __name__ == "__main__":
     print("Part 1 Test: ", solve('test_data.txt'))
     print("Part 1: ", solve('data.txt'))
 
-    #print("Part 2 Test:", solve('test_data.txt', part2=True))
-    #print("Part 2:", solve('data.txt', part2=True))
+    print("Part 2 Test (10):", solve('test_data.txt', 10))
+    print("Part 2 Test (100):", solve('test_data.txt', 100))
+    print("Part 2:", solve('data.txt', 1000000))
